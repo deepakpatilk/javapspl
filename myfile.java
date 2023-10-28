44fb7414146bb5cd02f644adbbd581d64924cb39
deepak patil <deepak.patil6760@gmail.com>
2023-10-28 09:36:11 +0000

    kkkk

--- myfile.java
+++ myfile.java
@@ -1,5 +1,5 @@
 class Simple{  
         public static void main(String args[]){  
-         System.out.println("Hello Deepak java testing");  
+         System.out.println("docker testing");  
         }  
     }  
